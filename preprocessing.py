024255659b97fb7885c83c6cd25fb16d03b3f7a0
Mahdizynali <zeinali.mahdy@gmail.com>
2025-04-21 00:32:59 +0330

    encapsulate DataPrep class

--- preprocessing.py
+++ preprocessing.py
@@ -2,38 +2,42 @@ import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-# نوع عواطف را دسته بندی می‌کنیم
-categories = {
-    "sad": 0,
-    "fear": 1,
-    "surprise": 2,
-    "disgust": 3,
-    "anger": 4,
-    "joy": 5
-}
+class DataPrep:
+    '''کلاس آماده سازی اولیه داده ها برای آموزش'''
+    def __init__(self, directory_path) -> None:
+        # نوع عواطف را دسته بندی می‌کنیم
+        self.categories = {
+            "sad": 0,
+            "fear": 1,
+            "surprise": 2,
+            "disgust": 3,
+            "anger": 4,
+            "joy": 5
+        }
+        self.data_path = directory_path
 
-def read_csv_dataset(directory_path):
-    csv_files = [f for f in os.listdir(directory_path) if f.endswith('.csv')]
-    dataframes = {}
+    def read_csv_dataset(self):
+        csv_files = [f for f in os.listdir(self.data_path) if f.endswith('.csv')]
+        dataframes = {}
 
-    for file in csv_files:
-        full_path = os.path.join(directory_path, file)
-        df = pd.read_csv(full_path)
+        for file in csv_files:
+            full_path = os.path.join(self.data_path, file)
+            df = pd.read_csv(full_path)
 
-        # فقط توییت و عواطف آن ذخیره شود
-        if 'tweet' in df.columns and 'emotion' in df.columns:
-            df = df[['tweet', 'emotion']]
-            df['emotion'] = df['emotion'].map(categories)
+            # فقط توییت و عواطف آن ذخیره شود
+            if 'tweet' in df.columns and 'emotion' in df.columns:
+                df = df[['tweet', 'emotion']]
+                df['emotion'] = df['emotion'].map(self.categories)
 
-        dataframes[file] = df
-    
-    # تمام دیتا ها را ترکیب و شافل می‌کنیم
-    merged_df = pd.concat(dataframes, ignore_index=True)
-    shuffled_df = merged_df.sample(frac=1, random_state=42).reset_index(drop=True)
-    return shuffled_df
+            dataframes[file] = df
+        
+        # تمام دیتا ها را ترکیب و شافل می‌کنیم
+        merged_df = pd.concat(dataframes, ignore_index=True)
+        shuffled_df = merged_df.sample(frac=1, random_state=42).reset_index(drop=True)
+        return shuffled_df
 
 
-dataframes = read_csv_dataset("dataset")
+dataframes = DataPrep("dataset").read_csv_dataset()
 # print(dataframes.head())
 
 # تقسیم داده ها با نسبت ۸۰ به ۲۰
