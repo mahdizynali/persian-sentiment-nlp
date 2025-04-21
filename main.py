d32e73731af35760eaf5cf76a0264852b33c1482
Mahdizynali <zeinali.mahdy@gmail.com>
2025-04-21 23:31:04 +0330

    simple test of tokenizer

--- main.py
+++ main.py
@@ -8,3 +8,15 @@ persian_label = {
     4: "عصبانیت",
     5: "ترکیبی"
 }
+
+
+data_prep = DataPrep(directory_path="dataset")
+
+X_train, X_test, y_train, y_test = data_prep.data_process()
+
+freqs = data_prep.freqs(X_train, y_train)
+# print(freqs)
+
+
+# for key, value in list(freqs.items())[:10]:
+#     print(f"Word-Sentiment Pair: {key}, Frequency: {value}")
